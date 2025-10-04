730e1ec05a4d58b9aca8b50cffd5dcd7bff061cb
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-04 16:00:58 +0000

    Füge weitere Karten ein unter dieser Überschrift in der 2. Zeile der Kar

--- src/app/_components/cooperation-partners.tsx
+++ src/app/_components/cooperation-partners.tsx
@@ -3,15 +3,12 @@ import Link from 'next/link';
 import {
   Card,
   CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
 } from '@/components/ui/card';
 import { OrthozentrumLogo } from '@/components/logos/orthozentrum-logo';
 import { AgnieszkaSlezakLogo } from '@/components/logos/agnieszka-slezak-logo';
 
 export function CooperationPartnersSection() {
-  const partners = [
+  const topPartners = [
     {
       name: 'VASC-ALLIANCE',
       logoUrl:
@@ -29,12 +26,27 @@ export function CooperationPartnersSection() {
       websiteUrl: 'https://www.orthozentrum-bern.ch/',
     },
   ];
-  const secondRowPartner = {
-    name: 'MCL',
-    logoUrl:
-      'https://www.mcl.ch/Portals/3/LOGO%20MCL%20DE%20WEB%20513X126.png?ver=cvc-6YjvCrM5oNcznlXoCQ%3d%3d',
-    websiteUrl: 'https://www.mcl.ch/',
-  };
+
+  const otherPartners = [
+    {
+        name: 'go-medical',
+        logoUrl: 'http://www.praxiszentrum-im-ring.ch//images/2024/08/18/go-medical-logo-600x117.png',
+        websiteUrl: '#'
+    },
+    {
+        name: 'MCL',
+        logoUrl:
+        'https://www.mcl.ch/Portals/3/LOGO%20MCL%20DE%20WEB%20513X126.png?ver=cvc-6YjvCrM5oNcznlXoCQ%3d%3d',
+        websiteUrl: 'https://www.mcl.ch/',
+    },
+    {
+        name: 'doxnet',
+        logoUrl: 'http://www.praxiszentrum-im-ring.ch//images/2024/08/18/doxnet_logo_rgb1.jpg',
+        websiteUrl: '#'
+    }
+  ];
+
+
   return (
     <section id="partners" className="w-full bg-primary">
       <div className="mx-auto w-full px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
@@ -42,7 +54,7 @@ export function CooperationPartnersSection() {
           Unsere ärztlichen Kooperationspartner
         </h2>
         <div className="mt-12 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4">
-          {partners.map((partner) => {
+          {topPartners.map((partner) => {
             return (
               <Link
                 key={partner.name}
@@ -58,7 +70,7 @@ export function CooperationPartnersSection() {
                         src={partner.logoUrl!}
                         alt={`${partner.name} Logo`}
                         width={340}
-                        height={153}
+                        height={100}
                         className="h-auto w-full object-contain"
                       />
                     ) : partner.name === 'Schemmer & Worni' ? (
@@ -89,36 +101,36 @@ export function CooperationPartnersSection() {
           })}
           <Card className="flex h-32 items-center justify-center p-6 text-center text-special-green">
             <CardContent className="p-0">
-              <AgnieszkaSlezakLogo className="h-16 w-auto" />
+              <AgnieszkaSlezakLogo className="h-20 w-auto" />
             </CardContent>
           </Card>
         </div>
         <h3 className="mt-16 text-center font-headline text-2xl font-bold tracking-tight text-primary-foreground sm:text-3xl">
           Unsere weiteren Partner
         </h3>
-        <div className="mt-8 flex justify-center">
-          <div className="w-full sm:w-1/2 lg:w-1/3">
-            <Link
-              key={secondRowPartner.name}
-              href={secondRowPartner.websiteUrl}
-              target={secondRowPartner.websiteUrl === '#' ? '_self' : '_blank'}
-              rel="noopener noreferrer"
-              className="group"
-            >
-              <Card className="flex h-32 items-center p-6 transition-all group-hover:-translate-y-1 group-hover:shadow-lg">
-                <CardContent className="flex w-full items-center justify-center p-0">
-                  <div className="relative flex h-[77px] w-full items-center justify-center overflow-hidden">
-                    <Image
-                      src={secondRowPartner.logoUrl!}
-                      alt={`${secondRowPartner.name} Logo`}
-                      fill
-                      className="object-contain"
-                    />
-                  </div>
-                </CardContent>
-              </Card>
-            </Link>
-          </div>
+        <div className="mt-8 grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
+            {otherPartners.map(partner => (
+                <Link
+                    key={partner.name}
+                    href={partner.websiteUrl}
+                    target={partner.websiteUrl === '#' ? '_self' : '_blank'}
+                    rel="noopener noreferrer"
+                    className="group"
+                >
+                    <Card className="flex h-32 items-center justify-center p-6 transition-all group-hover:-translate-y-1 group-hover:shadow-lg">
+                        <CardContent className="flex w-full items-center justify-center p-0">
+                             <div className="relative flex h-[77px] w-full items-center justify-center overflow-hidden">
+                                <Image
+                                src={partner.logoUrl!}
+                                alt={`${partner.name} Logo`}
+                                fill
+                                className="object-contain"
+                                />
+                            </div>
+                        </CardContent>
+                    </Card>
+                </Link>
+            ))}
         </div>
       </div>
     </section>
