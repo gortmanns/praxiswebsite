9a4654444ce1ae3fdfe7a2541ddd0ab9c51c4d6c
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-04 18:09:10 +0000

    alle 3 texte Linksbündig setzen.  Restliche Formatierung bleibt

--- src/app/notfall/page.tsx
+++ src/app/notfall/page.tsx
@@ -19,7 +19,7 @@ export default function NotfallPage() {
             <CardContent className="space-y-8 text-foreground/80">
               <div className="space-y-16">
                 
-                <div className="rounded-lg border border-destructive p-6">
+                <div className="border border-destructive p-6">
                   <div className="grid grid-cols-1 items-center gap-6 md:grid-cols-2">
                     <div className="mx-auto w-full max-w-lg">
                       <Image
@@ -31,11 +31,11 @@ export default function NotfallPage() {
                         data-ai-hint="ambulance"
                       />
                     </div>
-                    <div className="space-y-4 text-center">
+                    <div className="space-y-4 text-left">
                       <p className="text-2xl lg:text-3xl xl:text-5xl text-foreground">
                         Bei einem lebensbedrohlichen Notfall alarmieren Sie bitte unverzüglich den Rettungsdienst unter der Rufnummer
                       </p>
-                      <a href="tel:144" className="inline-flex w-full justify-center items-center gap-2 text-6xl lg:text-7xl xl:text-9xl font-bold text-destructive transition-colors hover:text-destructive/80">
+                      <a href="tel:144" className="inline-flex w-full justify-start items-center gap-2 text-6xl lg:text-7xl xl:text-9xl font-bold text-destructive transition-colors hover:text-destructive/80">
                         <Phone className="h-16 w-16 lg:h-20 lg:w-20 xl:h-24 xl:w-24" />
                         144
                       </a>
@@ -43,7 +43,7 @@ export default function NotfallPage() {
                   </div>
                 </div>
 
-                <div className="rounded-lg border border-border p-6">
+                <div className="border border-border p-6">
                   <div className="grid grid-cols-1 items-center gap-8 md:grid-cols-2">
                     <div className='mx-auto w-full max-w-lg'>
                       <Link href="https://www.medphone.ch/home" target="_blank" rel="noopener noreferrer">
@@ -57,14 +57,14 @@ export default function NotfallPage() {
                         />
                       </Link>
                     </div>
-                    <div className="space-y-4">
+                    <div className="space-y-4 text-left">
                         <p className='text-2xl lg:text-3xl xl:text-5xl'>Ausserhalb der Öffnungszeiten, ohne dass ein lebensbedrohlicher Notfall vorliegt, erhalten Sie medizinischen Rat durch die Hotline von Medphone:</p>
                         <div className='space-y-8 text-2xl lg:text-3xl xl:text-5xl'>
-                            <div className='flex flex-col'>
+                            <div className='flex flex-col items-start'>
                                 <a href="tel:0900576747" className='flex items-center gap-3'><Phone className='h-8 w-8 lg:h-10 lg:w-10 xl:h-12 xl:w-12 flex-shrink-0' /><strong className='font-bold'>0900 - 57 67 47</strong></a>
                                 <span className='text-lg lg:text-xl xl:text-2xl pl-12 lg:pl-14 xl:pl-16'>(3.23 CHF / min) nicht über Prepaid Handy</span>
                             </div>
-                            <div className='flex flex-col'>
+                            <div className='flex flex-col items-start'>
                                 <a href="tel:0900576748" className='flex items-center gap-3'><Phone className='h-8 w-8 lg:h-10 lg:w-10 xl:h-12 xl:w-12 flex-shrink-0' /><strong className='font-bold'>0900 - 57 67 48</strong></a>
                                 <span className='text-lg lg:text-xl xl:text-2xl pl-12 lg:pl-14 xl:pl-16'>(3.50 CHF / min) für Anruf über Prepaid Handy</span>
                             </div>
@@ -73,7 +73,7 @@ export default function NotfallPage() {
                   </div>
                 </div>
 
-                <div className="rounded-lg border border-border p-6">
+                <div className="border border-border p-6">
                   <div className="grid grid-cols-1 items-center gap-6 md:grid-cols-2">
                     <div className="mx-auto w-full max-w-lg">
                       <Image
@@ -84,11 +84,11 @@ export default function NotfallPage() {
                         className="h-auto w-full"
                       />
                     </div>
-                    <div className="space-y-4 text-center">
+                    <div className="space-y-4 text-left">
                       <p className="text-2xl lg:text-3xl xl:text-5xl text-foreground">
                         Bei Vergiftungen - aber nur wenn der Patient wach und ansprechbar und nicht in akuter Lebensgefahr ist - erreichen Sie den Vergiftungsnotruf unter der Nummer
                       </p>
-                      <a href="tel:145" className="inline-flex w-full justify-center items-center gap-2 text-6xl lg:text-7xl xl:text-9xl font-bold text-primary transition-colors hover:text-primary/80">
+                      <a href="tel:145" className="inline-flex w-full justify-start items-center gap-2 text-6xl lg:text-7xl xl:text-9xl font-bold text-primary transition-colors hover:text-primary/80">
                         <Phone className="h-16 w-16 lg:h-20 lg:w-20 xl:h-24 xl:w-24" />
                         145
                       </a>
