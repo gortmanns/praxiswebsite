83e4c777fce6548fdc1b1ba22863a737c3adc8f3
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-04 13:33:08 +0000

    Nimm die elemente der zeiten, teilweisen Figur und füge sie gespiegelt a

--- src/app/_components/quick-nav-section.tsx
+++ src/app/_components/quick-nav-section.tsx
@@ -19,10 +19,15 @@ function UsersIcon(props: React.SVGProps<SVGSVGElement>) {
       strokeLinejoin="round"
       {...props}
     >
+      {/* Original main figure */}
       <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
       <circle cx="9" cy="7" r="4" />
+      {/* Original partial figure on the right */}
       <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
       <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+      {/* Mirrored partial figure on the left */}
+      <path d="M-2 21v-2a4 4 0 0 1 3-3.87" />
+      <path d="M2 3.13a4 4 0 0 0 0 7.75" />
     </svg>
   );
 }
