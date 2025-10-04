5ec0d42400c7460528704cf45de3598469ed2994
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-04 14:44:14 +0000

    Der Abstand stimmt aber noch immer ist das halbe n am Ende abgeschnitten

--- src/app/_components/cooperation-partners.tsx
+++ src/app/_components/cooperation-partners.tsx
@@ -6,7 +6,7 @@ const ZahnradIcon = () => (
   <svg
     width="250"
     height="56"
-    viewBox="0 0 260 56"
+    viewBox="0 0 285 56"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
     aria-label="Orthozentrum Bern Logo"
