f0a7af1ac4fbc7348a31e35ccfc2e925e46cceae
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-04 13:50:44 +0000

    <html> <head> <title>Font Awesome 5 Icons</title> <meta name='viewport'

--- src/app/_components/quick-nav-section.tsx
+++ src/app/_components/quick-nav-section.tsx
@@ -3,7 +3,6 @@ import Link from 'next/link';
 import {
   AlertTriangle,
   Users,
-  HandHelping,
 } from 'lucide-react';
 
 const navItems = [
@@ -15,7 +14,7 @@ const navItems = [
   {
     href: '/leistungen',
     label: 'Leistungen',
-    Icon: HandHelping,
+    Icon: FaHandHoldingMedicalIcon,
   },
   {
     href: '/medikamente',
@@ -48,6 +47,20 @@ function FaPillsIcon(props: React.SVGProps<SVGSVGElement>) {
     </svg>
   );
 }
+
+function FaHandHoldingMedicalIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 640 512"
+      fill="currentColor"
+      {...props}
+    >
+      <path d="M96 128a128 128 0 1 0 256 0A128 128 0 1 0 96 128zM32 128C32 57.31 89.31 0 160 0s128 57.31 128 128c0 31.25-11.31 60-30.16 82.25L174.4 340.8C165.6 359.3 146.5 369.6 126.4 369.6C100.2 369.6 79.13 354.4 70.21 332.8L3.461 165.9C1.223 159.8 0 153.3 0 146.8V128c0-14.14 11.46-25.6 25.6-25.6H32zM640 176v16c0 35.35-28.65 64-64 64H512V128h64c35.35 0 64 28.65 64 64zM512 320h64c17.67 0 32 14.33 32 32v64c0 17.67-14.33 32-32 32h-64v64h64c53.02 0 96-42.98 96-96v-64c0-53.02-42.98-96-96-96h-64v64zM494.5 353.7l-42.38-42.38c-6.252-6.252-16.38-6.252-22.63 0s-6.252 16.38 0 22.63L452.1 356.5l-42.38 42.38c-6.252 6.252-6.252 16.38 0 22.63s16.38 6.252 22.63 0L497.1 379.1l42.38 42.38c6.252 6.252 16.38 6.252 22.63 0s6.252-16.38 0-22.63L517.3 356.5l42.38-42.38c6.252-6.252 6.252-16.38 0-22.63s-16.38-6.252-22.63 0L494.7 333.9z" />
+    </svg>
+  );
+}
+
 
 export function QuickNavSection() {
   return (
