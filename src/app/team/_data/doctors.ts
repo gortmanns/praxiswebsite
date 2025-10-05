a76cfd0580af5212d475969eb22bbf46c5be753b
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-05 15:51:23 +0000

    Warum ist es für Dich offenbar nicht möglich, dass Du ienfach nur genau

--- src/app/team/_data/doctors.ts
+++ src/app/team/_data/doctors.ts
@@ -79,12 +79,6 @@ export const doctors: Doctor[] = [
       {
         point: 'Grosse chirurgische Eingriffe werden in enger Kooperation zwischen Chirurg und Hausarzt durchgeführt.',
         isList: true,
-        subPoints: [
-            'Vorbesprechung und Planung des Eingriffs erfolgen hier im PRAXISZENTRUM IM RING.',
-            'Allenfalls notwendige Abklärungen vor dem Eingriff finden ebenfalls hier im Praxiszentrum statt oder – falls nötig – per Überweisung an weitere Spezialisten.',
-            'Die Operation selbst findet in einer der Partnerkliniken in der Stadt Bern statt.',
-            'Die Nachbetreuung (z. B. Fadenentfernung und Schmerzbehandlung) findet wieder hier vor Ort im PRAXISZENTRUM IM RING statt.',
-        ],
       },
       {
         point: 'Vorbesprechung und Planung des Eingriffs erfolgen hier im PRAXISZENTRUM IM RING.',
@@ -98,13 +92,17 @@ export const doctors: Doctor[] = [
         point: 'Die Operation selbst findet in einer der Partnerkliniken in der Stadt Bern statt.',
         isParagraph: true,
         subPoints: [
-            'Klinik Beau-Site, Bern',
-            'SALEM-SPITAL, Bern',
-            'Hirslanden-Klinik, Bern',
+          'Klinik Beau-Site, Bern',
+          'SALEM-SPITAL, Bern',
+          'Hirslanden-Klinik, Bern',
         ],
       },
       {
         point: 'Die Nachbetreuung (z. B. Fadenentfernung und Schmerzbehandlung) findet wieder hier vor Ort im PRAXISZENTRUM IM RING statt.',
+        isParagraph: true,
+      },
+       {
+        point: 'Die Operation selbst findet in einer der Partnerkliniken in der Stadt Bern statt.',
         isParagraph: true,
       },
     ],
