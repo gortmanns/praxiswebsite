42e5833f49872835c203f407172d4769cc28241b
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-05 16:40:34 +0000

    Du hast die Schmmer Karte verändert und dort das Logo entfernt. Ich habe

--- src/app/team/_components/doctor-card-schemmer.tsx
+++ src/app/team/_components/doctor-card-schemmer.tsx
@@ -31,7 +31,7 @@ export const DoctorCardSchemmer = () => (
                                 <div className="mt-[1.5cqw] text-[2.2cqw] leading-tight">
                                     <p className="font-bold">Facharzt für Allgemein- und Viszeralchirurgie</p>
                                 </div>
-                                <div className="relative h-[10cqw] w-[30cqw]">
+                                <div className="relative mt-[2.5cqw] h-[10cqw] w-[30cqw]">
                                   <Image
                                     src="/images/schemmer-worni-logo.png"
                                     alt="Schemmer & Worni Logo"
@@ -46,11 +46,11 @@ export const DoctorCardSchemmer = () => (
                 </CardContent>
                 <div className="absolute inset-0 flex flex-col items-start justify-center overflow-auto bg-accent p-6 text-left text-background opacity-0 transition-opacity duration-300 group-hover:opacity-100">
                     <ul className="space-y-1.5 text-[clamp(0.8rem,2.5cqw,1.2rem)] leading-tight">
-                        <li className="font-bold text-primary mt-2 mb-4">KLEINCHIRURGISCHE EINGRIFFE KÖNNEN DIREKT VOR ORT IM PRAXISZENTRUM IM RING ERFOLGEN</li>
-                        <li className="font-bold text-primary mt-2 mb-4">GROSSE CHIRURGISCHE EINGRIFFE WERDEN IN ENGER KOOPERATION ZWISCHEN CHIRURG UND HAUSARZT DURCHGEFÜHRT</li>
-                        <li className="mt-6 mb-4">Die Vorbesprechung und Planung des Eingriffs erfolgen im PRAXISZENTRUM IM RING.</li>
-                        <li>Allenfalls notwendige Abklärungen vor dem Eingriff finden ebenfalls im Praxiszentrum statt oder – falls nötig – per Überweisung an weitere Spezialisten.</li>
-                        <li>
+                        <li className="font-bold text-primary mt-6 mb-4">KLEINCHIRURGISCHE EINGRIFFE KÖNNEN DIREKT VOR ORT IM PRAXISZENTRUM IM RING ERFOLGEN</li>
+                        <li className="font-bold text-primary mt-6 mb-4">GROSSE CHIRURGISCHE EINGRIFFE WERDEN IN ENGER KOOPERATION ZWISCHEN CHIRURG UND HAUSARZT DURCHGEFÜHRT</li>
+                        <li className="mb-4">Die Vorbesprechung und Planung des Eingriffs erfolgen im PRAXISZENTRUM IM RING.</li>
+                        <li className="mb-4">Allenfalls notwendige Abklärungen vor dem Eingriff finden ebenfalls im Praxiszentrum statt oder – falls nötig – per Überweisung an weitere Spezialisten.</li>
+                        <li className="mb-4">
                             Die Operation selbst findet in einer der Partnerkliniken in der Stadt Bern statt.
                             <ul className="list-disc space-y-px pl-9 pt-1 text-[clamp(0.7rem,2.3cqw,1rem)] font-normal text-background/80">
                                 <li>Hirslanden</li>
@@ -58,7 +58,7 @@ export const DoctorCardSchemmer = () => (
                                 <li>Siloah-Spital</li>
                             </ul>
                         </li>
-                        <li className="mt-6 mb-4">Die Nachbetreuung (z. B. Fadenentfernung und Schmerzbehandlung) findet wieder vor Ort im PRAXISZENTRUM IM RING statt.</li>
+                        <li className="mb-4">Die Nachbetreuung (z. B. Fadenentfernung und Schmerzbehandlung) findet wieder vor Ort im PRAXISZENTRUM IM RING statt.</li>
                     </ul>
                 </div>
             </Card>
