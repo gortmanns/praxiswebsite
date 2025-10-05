21d1d6574211be28962957785497360b17860253
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-05 17:01:45 +0000

    Kopiere die Card Rosenov, füge sie unerhalb erneut ein aber mit Herschel

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,4 +1,42 @@
 
+
+import { Header } from '../_components/header';
+import { Footer } from '../_components/footer';
+import { DoctorCardOrtmanns } from './_components/doctor-card-ortmanns';
+import { DoctorCardSchemmer } from './_components/doctor-card-schemmer';
+import { DoctorCardRosenov } from './_components/doctor-card-rosenov';
+import { DoctorCardHerschel } from './_components/doctor-card-herschel';
+
+export default function TeamPage() {
+  return (
+    <div className="flex min-h-screen flex-col bg-background">
+      <Header />
+      <main className="flex-1">
+        <div className="container py-16 sm:py-24">
+          <div className="mx-auto mt-16 max-w-5xl text-center">
+            <h2 className="font-headline text-3xl font-bold tracking-tight text-primary sm:text-4xl sm:whitespace-nowrap">
+              UNSER TEAM
+            </h2>
+          </div>
+          <div className="mx-auto mt-8 max-w-5xl space-y-8">
+            <div>
+              <h3 className="font-headline text-2xl font-bold tracking-tight text-primary sm:text-3xl">
+                  Ärzte
+              </h3>
+              <div className="mt-2 h-1 w-full bg-primary"></div>
+            </div>
+            <DoctorCardOrtmanns />
+            <DoctorCardSchemmer />
+            <DoctorCardRosenov />
+            <DoctorCardHerschel />
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+{/*
 import { Header } from '../_components/header';
 import { Footer } from '../_components/footer';
 import { DoctorCardOrtmanns } from './_components/doctor-card-ortmanns';
@@ -33,3 +71,4 @@ export default function TeamPage() {
     </div>
   );
 }
+*/}
