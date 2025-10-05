9e5573665c7e4b63fc2ea537bd4016e18c362278
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-05 18:52:23 +0000

    Nein, dieses ist NICHT das korrekte Bild. Suche nochmal nach dem richtig

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -51,7 +51,7 @@ const otherTeamMembers = [
     {
       name: 'G. Öztürk',
       role: 'Praxishilfe',
-      imageUrl: '/images/team/Oertmann.jpg',
+      imageUrl: '', // LEER GELASSEN, UM FEHLER ZU VERMEIDEN
       imageHint: 'man portrait',
       backsideContent: (
         <>
