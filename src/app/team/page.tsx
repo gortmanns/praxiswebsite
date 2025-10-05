5f3fd2bca7ab11438c1bdf4079793234e149c319
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-05 15:08:04 +0000

    Du hast es nicht wiederhergestellt. Was haben wir über änderungen verein

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -46,7 +46,7 @@ const DoctorCard = () => (
                 </div>
                 </CardContent>
                 <div className="absolute inset-0 flex flex-col items-start justify-center overflow-auto bg-accent p-6 text-left text-background opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-                <ul className="space-y-1.5 text-[clamp(0.8rem,2.2cqw,1.2rem)] leading-tight">
+                <ul className="space-y-1.5 text-[clamp(0.8rem,2.3cqw,1.2rem)] leading-tight">
                     <li className="font-bold text-primary">
                     Medizinstudium in Bonn (Deutschland) und Hobart
                     (Australien)
@@ -62,7 +62,7 @@ const DoctorCard = () => (
                     <li className="font-bold text-primary">
                     Projektmanagement im Gesundheitswesen in Europa und
                     Australien
-                    <div className="mt-1 pl-4 text-[clamp(0.7rem,2cqw,1rem)] leading-snug text-background/80">
+                    <div className="mt-1 pl-4 text-[clamp(0.7rem,2.1cqw,1rem)] leading-snug text-background/80">
                         <h5 className="mb-1 font-bold uppercase tracking-wider text-primary">
                         Meilensteine
                         </h5>
@@ -88,7 +88,7 @@ const DoctorCard = () => (
                     </li>
                     <li className="mt-2 font-bold text-primary">
                     Weiterbildung in Allgemeiner Innerer Medizin in der Schweiz
-                    <ul className="mt-1 list-disc space-y-px pl-9 text-[clamp(0.7rem,2cqw,1rem)] leading-snug text-background/80">
+                    <ul className="mt-1 list-disc space-y-px pl-9 text-[clamp(0.7rem,2.1cqw,1rem)] leading-snug text-background/80">
                         <li>Universitätsspital Basel (USB)</li>
                         <li>Kantonsspital Baselland (KSBL)</li>
                         <li>Kantonsspital Winterthur (KSW)</li>
@@ -97,11 +97,11 @@ const DoctorCard = () => (
                     </ul>
                     </li>
                 </ul>
-                <p className="mt-2 text-[clamp(0.7rem,2cqw,1rem)] font-bold text-primary">
+                <p className="mt-2 text-[clamp(0.7rem,2.1cqw,1rem)] font-bold text-primary">
                     Wissenschaftlicher Mitarbeiter an der Universität Zürich /
                     USZ (Abteilung für Pneumologie)
                 </p>
-                <p className="text-[clamp(0.7rem,2cqw,1rem)] font-bold text-primary">
+                <p className="text-[clamp(0.7rem,2.1cqw,1rem)] font-bold text-primary">
                     Lehrbeauftragter für Hausarztmedizin (Institut für
                     Hausarztmedizin der Universität Bern)
                 </p>
