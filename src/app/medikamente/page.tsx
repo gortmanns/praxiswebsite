c268aabf691c5ada1ff90262212906f98db8706f
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-05 23:25:54 +0000

    Definiere eine Obergrenze für die Skalierung des Bildes. Dieses sollte n

--- src/app/medikamente/page.tsx
+++ src/app/medikamente/page.tsx
@@ -15,30 +15,32 @@ export default function MedikamentePage() {
               MEDIKAMENTE
             </h2>
           </div>
-          <Card className="mt-16">
-            <CardContent className="p-6">
-              <div className="grid grid-cols-1 gap-12 md:grid-cols-2">
-                <div className="relative aspect-[3/2] w-full">
-                  <Image
-                    src="/images/foto-medis.jpg"
-                    alt="Verschiedene Medikamentenschachteln"
-                    data-ai-hint="medication boxes"
-                    fill
-                    className="rounded-lg object-cover"
-                  />
+          <div className="mx-auto mt-16 max-w-7xl">
+            <Card>
+              <CardContent className="p-6">
+                <div className="grid grid-cols-1 items-center gap-12 md:grid-cols-2">
+                  <div className="relative aspect-[3/2] w-full">
+                    <Image
+                      src="/images/foto-medis.jpg"
+                      alt="Verschiedene Medikamentenschachteln"
+                      data-ai-hint="medication boxes"
+                      fill
+                      className="rounded-lg object-cover"
+                    />
+                  </div>
+                  <div className="flex flex-col justify-center space-y-4">
+                    <h3 className="font-headline text-2xl font-bold text-primary">Ihre Medikamente direkt vom Hausarzt</h3>
+                    <p className="text-lg text-foreground/80">
+                      Als Patient können Sie alle Ihre Medikamente bequem bei uns beziehen. So sparen Sie nicht nur Geld sondern Sie haben auch die Gewissheit, dass Sie wirklich nur die Medikamente erhalten und einnehmen, die Sie auch benötigen. Bei jeder Abgabe eines Medikaments wird dieses mit den in Ihrer Krankenakte hinterlegten Medikamenten abgeglichen.
+                    </p>
+                    <p className="text-lg text-foreground/80">
+                      Um Ihnen den Bezug der Medikamente so einfach wie möglich zu machen, bieten wir Ihnen die Möglichkeit, die Medikamente vorzubestellen.
+                    </p>
+                  </div>
                 </div>
-                <div className="flex flex-col justify-center space-y-4">
-                  <h3 className="font-headline text-2xl font-bold text-primary">Ihre Medikamente direkt vom Hausarzt</h3>
-                  <p className="text-lg text-foreground/80">
-                    Als Patient können Sie alle Ihre Medikamente bequem bei uns beziehen. So sparen Sie nicht nur Geld sondern Sie haben auch die Gewissheit, dass Sie wirklich nur die Medikamente erhalten und einnehmen, die Sie auch benötigen. Bei jeder Abgabe eines Medikaments wird dieses mit den in Ihrer Krankenakte hinterlegten Medikamenten abgeglichen.
-                  </p>
-                  <p className="text-lg text-foreground/80">
-                    Um Ihnen den Bezug der Medikamente so einfach wie möglich zu machen, bieten wir Ihnen die Möglichkeit, die Medikamente vorzubestellen.
-                  </p>
-                </div>
-              </div>
-            </CardContent>
-          </Card>
+              </CardContent>
+            </Card>
+          </div>
         </div>
       </main>
       <Footer />
