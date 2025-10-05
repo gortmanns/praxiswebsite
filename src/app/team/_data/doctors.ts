1d672ce22e25ebf9ab5aea7334a509a4868af987
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-05 15:48:47 +0000

    Habe ich Dir gesagt, dass Du "Die Operation selbst...." zu einer blauen

--- src/app/team/_data/doctors.ts
+++ src/app/team/_data/doctors.ts
@@ -76,7 +76,6 @@ export const doctors: Doctor[] = [
       },
       {
         point: 'Grosse chirurgische Eingriffe werden in enger Kooperation zwischen Chirurg und Hausarzt durchgeführt.',
-        isList: true,
         subPoints: [
           'Vorbesprechung und Planung des Eingriffs erfolgen hier im PRAXISZENTRUM IM RING.',
           'Allenfalls notwendige Abklärungen vor dem Eingriff finden ebenfalls hier im Praxiszentrum statt oder – falls nötig – per Überweisung an weitere Spezialisten.',
@@ -86,7 +85,6 @@ export const doctors: Doctor[] = [
       },
       {
         point: 'Die Operation selbst findet in einer der Partnerkliniken in der Stadt Bern statt.',
-        isList: true,
         subPoints: [
           'Klinik Beau-Site, Bern',
           'SALEM-SPITAL, Bern',
