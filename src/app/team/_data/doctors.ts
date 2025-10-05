67f10ba1c9216e8bf32e1d111c3092834ae2348c
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-05 15:33:08 +0000

    Das bild wird nicht dargestellt. Durchsuche den Ordner Team und finde di

--- src/app/team/_data/doctors.ts
+++ src/app/team/_data/doctors.ts
@@ -69,7 +69,7 @@ export const doctors: Doctor[] = [
         'Facharzt für Allgemein- und Viszeralchirurgie',
     ],
     position: 'Schemmer • Worni\nKlinik für Allgemein- und Viszeralchirurgie',
-    imageUrl: '/images/team/Schemmer.jpg',
+    imageUrl: '/images/team/Ortmanns.jpg',
     cv: [
         {
             point: 'Facharzt für Allgemein- und Viszeralchirurgie mit umfassender Erfahrung in leitenden Positionen.',
