c9259d9ec353e1e03907b162995302445ed8137f
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-05 16:41:40 +0000

    Hast Du vor der Änderung dafür gesorgt, dass Du alle Änderungen exakt na

--- src/app/team/_components/doctor-card-schemmer.tsx
+++ src/app/team/_components/doctor-card-schemmer.tsx
@@ -45,9 +45,13 @@ export const DoctorCardSchemmer = () => (
                     </div>
                 </CardContent>
                 <div className="absolute inset-0 flex flex-col items-start justify-center overflow-auto bg-accent p-6 text-left text-background opacity-0 transition-opacity duration-300 group-hover:opacity-100">
-                    <ul className="space-y-1.5 text-[clamp(0.8rem,2.5cqw,1.2rem)] leading-tight">
-                        <li className="font-bold text-primary mt-6 mb-4">KLEINCHIRURGISCHE EINGRIFFE KÖNNEN DIREKT VOR ORT IM PRAXISZENTRUM IM RING ERFOLGEN</li>
-                        <li className="font-bold text-primary mt-6 mb-4">GROSSE CHIRURGISCHE EINGRIFFE WERDEN IN ENGER KOOPERATION ZWISCHEN CHIRURG UND HAUSARZT DURCHGEFÜHRT</li>
+                <ul className="space-y-1.5 text-[clamp(0.8rem,2.5cqw,1.2rem)] leading-tight">
+                        <li className="font-bold text-primary mt-6 mb-4">
+                            KLEINCHIRURGISCHE EINGRIFFE KÖNNEN DIREKT VOR ORT IM PRAXISZENTRUM IM RING ERFOLGEN
+                        </li>
+                        <li className="font-bold text-primary mt-6 mb-4">
+                            GROSSE CHIRURGISCHE EINGRIFFE WERDEN IN ENGER KOOPERATION ZWISCHEN CHIRURG UND HAUSARZT DURCHGEFÜHRT
+                        </li>
                         <li className="mb-4">Die Vorbesprechung und Planung des Eingriffs erfolgen im PRAXISZENTRUM IM RING.</li>
                         <li className="mb-4">Allenfalls notwendige Abklärungen vor dem Eingriff finden ebenfalls im Praxiszentrum statt oder – falls nötig – per Überweisung an weitere Spezialisten.</li>
                         <li className="mb-4">
