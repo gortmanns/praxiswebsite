9da2c290b846c6dc27769e5f97906746e00253f6
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-06 21:32:02 +0000

    gut. Ich bin im Dashboard. Nun Setzte eine weiterleitung von /admin die

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ require('dotenv').config({ path: './.env' });
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/admin',
+        destination: '/admin/login',
+        permanent: true,
+      },
+    ]
+  },
   /* config options here */
   env: {
     ADMIN_USERNAME: process.env.ADMIN_USERNAME,
