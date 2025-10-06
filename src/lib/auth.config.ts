9dea359154bc12d4930ad050e37745a0a88c8767
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-06 20:48:08 +0000

    Klick auf Ferientermine - sofort wieder login-seite

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -8,15 +8,20 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/admin/dashboard');
+      
       if (isOnDashboard) {
-        if (isLoggedIn) return true;
+        // If the user is trying to access the dashboard or its sub-pages
+        if (isLoggedIn) return true; // Allow access if logged in
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-         if (nextUrl.pathname.startsWith('/admin')) {
-            return true;
-         }
-         // This can be adjusted if you want to redirect logged-in users from other pages
+        // If the user is logged in and tries to access the login page,
+        // redirect them to the dashboard.
+        if (nextUrl.pathname.startsWith('/admin')) {
+           return Response.redirect(new URL('/admin/dashboard', nextUrl));
+        }
       }
+      
+      // Allow all other requests (e.g. for the public site)
       return true;
     },
   },
