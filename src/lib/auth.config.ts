d35d5ad88dbbe1787a8ca7781fe3f891581fb5f6
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-06 20:45:50 +0000

    nun komme ich bei Login wieder auf die Startseite

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -8,25 +8,15 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/admin/dashboard');
-      
       if (isOnDashboard) {
-        if (isLoggedIn) {
-            // Check if the session has expired
-            if (auth.expires && new Date(auth.expires) < new Date()) {
-                // Session expired, redirect to login
-                return false;
-            }
-            return true;
-        }
+        if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        // If logged in and trying to access the login page, redirect to dashboard
+        // Redirect logged-in users trying to access the login page to the dashboard
         if (nextUrl.pathname === '/admin') {
-            return Response.redirect(new URL('/admin/dashboard', nextUrl));
+          return Response.redirect(new URL('/admin/dashboard', nextUrl));
         }
       }
-      
-      // Allow access to all other pages (e.g., login page for unauthenticated users)
       return true;
     },
   },
