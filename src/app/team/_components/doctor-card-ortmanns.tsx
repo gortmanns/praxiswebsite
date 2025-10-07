4af08dfd0d7de167ecc22e7ae23fb54695073ced
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-07 03:24:02 +0000

    der abstand ist noch immer nicht glecih

--- src/app/team/_components/doctor-card-ortmanns.tsx
+++ src/app/team/_components/doctor-card-ortmanns.tsx
@@ -45,14 +45,15 @@ export const DoctorCardOrtmanns = () => {
                         <div className="flex w-full flex-col space-y-3 text-[clamp(0.8rem,2.5cqw,1.1rem)] leading-tight">
                             <p>Medizinstudium in Bonn und Hobart (Australien)</p>
                             <p>Masterstudium Public Health und Health Management in Sydney (Australien)</p>
-                            <p>Projektmanagement im Gesundheitswesen in Europa und Australien</p>
-                            <p>Unternehmensberatung mit Spezialisierung auf den Gesundheitssektor</p>
-                            
-                            <ul className="flex list-disc flex-col pl-5 pt-1 text-sm text-background/80" style={{ gap: '0.15rem' }}>
-                                <li>Leiter Klinische Entwicklung und Analytik für DxCG Gesundheitsanalytik GmbH (Deutschland)</li>
-                                <li>Manager für Klinische Sicherheit und Design Assurance bei der Entwicklung der Nationalen Elektronischen Gesundheitsakte in Australien</li>
-                                <li>Direktor der Memory-Strategie (elektronisches Medikamenten-management und elektronische Patientenakten) für das Netzwerk der Kinderkrankenhäuser in Sydney, Australien</li>
-                            </ul>
+                            <div>
+                                <p>Projektmanagement im Gesundheitswesen in Europa und Australien</p>
+                                <p>Unternehmensberatung mit Spezialisierung auf den Gesundheitssektor</p>
+                                <ul className="flex list-disc flex-col pl-5 pt-1 text-sm text-background/80" style={{ gap: '0.15rem' }}>
+                                    <li>Leiter Klinische Entwicklung und Analytik für DxCG Gesundheitsanalytik GmbH (Deutschland)</li>
+                                    <li>Manager für Klinische Sicherheit und Design Assurance bei der Entwicklung der Nationalen Elektronischen Gesundheitsakte in Australien</li>
+                                    <li>Direktor der Memory-Strategie (elektronisches Medikamenten-management und elektronische Patientenakten) für das Netzwerk der Kinderkrankenhäuser in Sydney, Australien</li>
+                                </ul>
+                            </div>
                             
                             <div>
                                 <p>Weiterbildung in Allgemeiner Innerer Medizin in der Schweiz:</p>
