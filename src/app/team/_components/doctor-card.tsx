f7750046ada8e8c1f2c25df6381af8ce58e23c1e
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-09 00:48:04 +0000

    nun ist es nicht mehr links sondern nach rechts gerückt, das war nicht g

--- src/app/team/_components/doctor-card.tsx
+++ src/app/team/_components/doctor-card.tsx
@@ -55,7 +55,7 @@ export const DoctorCard: React.FC<DoctorCardProps> = ({
                                     </div>
                                     
                                     {children && (
-                                        <div className="relative mt-[2.5cqw] w-full">
+                                        <div className="relative mt-[2.5cqw] flex w-full justify-start">
                                             {children}
                                         </div>
                                     )}
