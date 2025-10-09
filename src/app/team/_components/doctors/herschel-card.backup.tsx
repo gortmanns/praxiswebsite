050636c521ccf4b3f008c6fa229bf65e7a379951
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-09 01:56:48 +0000

    Gut. Nun alle Karten in ihre jeweilige Master-Kopie schreiben. Wir werde

---
+++ src/app/team/_components/doctors/herschel-card.backup.tsx
@@ -0,0 +1,28 @@
+
+'use client';
+
+import { DoctorCard } from '../doctor-card';
+import { OrthozentrumLogo } from '@/components/logos/orthozentrum-logo';
+
+export function HerschelCard() {
+    return (
+        <DoctorCard
+            title="Dr. med."
+            name="R. Herschel"
+            imageUrl="/images/team/Dr.Herschel.jpg"
+            imageHint="man portrait"
+            specialty={
+             <div>
+                <span>Facharzt Orthopädische Chirurgie und</span>
+                <span>Traumatologie des Bewegungsapparates</span>
+             </div>
+            }
+            qualifications={[]}
+            vita={`
+                <p>Vita folgt in Kürze.</p>
+            `}
+        >
+            <OrthozentrumLogo className="h-28 w-auto" />
+        </DoctorCard>
+    );
+}
---
+++ src/app/team/_components/doctors/slezak-card.backup.tsx
@@ -0,0 +1,23 @@
+
+'use client';
+
+import { DoctorCard } from '../doctor-card';
+import { AgnieszkaSlezakLogo } from '@/components/logos/agnieszka-slezak-logo';
+
+export function SlezakCard() {
+    return (
+        <DoctorCard
+            title="Dr. med."
+            name="A. Slezak"
+            imageUrl="/images/team/Dr.Slezak.jpg"
+            imageHint="woman portrait"
+            specialty="Fachärztin für Neurologie"
+            qualifications={[]}
+            vita={`
+                <p>Vita folgt in Kürze.</p>
+            `}
+        >
+            <AgnieszkaSlezakLogo className="h-[6.9rem] w-auto" />
+        </DoctorCard>
+    );
+}
