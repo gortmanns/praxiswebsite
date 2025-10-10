925d9c770a16d7faee5601bad8505517a005f660
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-10 23:57:44 +0000

    Nun 2. Zeile fett und näher an die erste heranrücken

--- src/app/team/_components/doctors/slezak-card.tsx
+++ src/app/team/_components/doctors/slezak-card.tsx
@@ -31,7 +31,7 @@ export function SlezakCard() {
                                             .slezak-text-normal { fill: #358392; font-family: 'Montserrat', sans-serif; font-weight: normal; }
                                         </style>
                                         <text x="50%" y="20" text-anchor="middle" class="slezak-text-special" font-size="14">Dr. med. A. Slezak</text>
-                                        <text x="50%" y="45" text-anchor="middle" class="slezak-text-normal" font-size="7">Fachärztin für Neurologie</text>
+                                        <text x="50%" y="40" text-anchor="middle" class="slezak-text-normal" font-size="7" font-weight="bold">Fachärztin für Neurologie</text>
                                     </svg>
                                 </div>
                             </div>
