2744026b0126eb762bb1f9451817aa0dc7da992e
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-10 22:55:41 +0000

    Aha. Habe ich Dich also ertappt. Ich weiss warum es nicht gelingt die Ka

--- src/app/team/_components/doctors/herschel-card.tsx
+++ src/app/team/_components/doctors/herschel-card.tsx
@@ -1,28 +1,28 @@
+
 'use client';
 
 import { DoctorCard } from '../doctor-card';
 import { OrthozentrumLogo } from '@/components/logos/orthozentrum-logo';
 
-export const herschelProps = {
-    id: "herschel",
-    order: 4,
-    title: "Dr. med.",
-    name: "R. Herschel",
-    imageUrl: "/images/team/Dr.Herschel.jpg",
-    imageHint: "man portrait",
-    specialty: (
-        <div>
-            <span>Facharzt Orthopädische Chirurgie und</span>
-            <br/>
-            <span>Traumatologie des Bewegungsapparates</span>
-        </div>
-    ),
-    qualifications: [],
-    vita: `<p>Vita folgt in Kürze.</p>`,
-    partnerLogoComponent: OrthozentrumLogo,
-    languages: ['de', 'fr', 'it', 'en', 'es'],
-};
-
 export function HerschelCard() {
-    return <DoctorCard {...herschelProps} />;
+    return (
+        <DoctorCard
+            title="Dr. med."
+            name="R. Herschel"
+            imageUrl="/images/team/Dr.Herschel.jpg"
+            imageHint="man portrait"
+            specialty={
+             <div>
+                <span>Facharzt Orthopädische Chirurgie und</span>
+                <span>Traumatologie des Bewegungsapparates</span>
+             </div>
+            }
+            qualifications={[]}
+            vita={`
+                <p>Vita folgt in Kürze.</p>
+            `}
+        >
+            <OrthozentrumLogo className="h-28 w-auto" />
+        </DoctorCard>
+    );
 }
