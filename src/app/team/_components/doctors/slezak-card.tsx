cbbee4821d5a91ea3cd3d1ecacb7dc85774fdcff
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-10 23:07:47 +0000

    Überprüfe dass die Karten 1000:495px gross sind. Die Fotos bekommen das

--- src/app/team/_components/doctors/slezak-card.tsx
+++ src/app/team/_components/doctors/slezak-card.tsx
@@ -12,7 +12,7 @@ export function SlezakCard() {
             frontSideCode={`
                 <div class="group relative w-full max-w-[1000px] aspect-[1000/495] overflow-hidden rounded-lg shadow-sm">
                     <div class="absolute inset-0 flex items-center justify-start bg-card text-card-foreground p-12">
-                        <div class="relative w-[330px] h-full overflow-hidden rounded-md">
+                        <div class="relative w-[330px] h-full overflow-hidden rounded-md aspect-[2/3]">
                             <img src="/images/team/Dr.Slezak.jpg" alt="Portrait von A. Slezak" data-ai-hint="woman portrait" class="w-full h-full object-cover" />
                         </div>
                         <div class="ml-12 flex flex-col justify-start text-left h-full">
