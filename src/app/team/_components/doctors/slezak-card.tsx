92c69579d5488acbe39bbeadcca45046428d0d73
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-11 00:30:34 +0000

    Entferne bei Slezak alle Farbsymbole ausser deutsch

--- src/app/team/_components/doctors/slezak-card.tsx
+++ src/app/team/_components/doctors/slezak-card.tsx
@@ -24,8 +24,8 @@ export function SlezakCard() {
                                 <h3 class="text-5xl font-bold text-primary my-2">A. Slezak</h3>
                                 <p class="text-xl font-bold">Fachärztin für Neurologie</p>
                                 <div class="mt-6 flex items-center justify-start">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 280 60" class="h-auto" style="height: 6.9rem;" aria-label="Dr. med. Agnieszka Slezak - Fachärztin für Neurologie Logo">
-                                    <rect width="280" height="60" fill="#6E7F6C" rx="6"></rect>
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 60" class="h-auto" style="height: 6.9rem;" aria-label="Dr. med. Agnieszka Slezak - Fachärztin für Neurologie Logo">
+                                    <rect width="320" height="60" fill="#6E7F6C" rx="6"></rect>
                                     <text x="50%" y="24" dominant-baseline="middle" text-anchor="middle" style="font-family: Montserrat, sans-serif; font-size: 14px; font-weight: bold; fill: white;">
                                         Dr. med. Agnieszka Slezak
                                     </text>
@@ -37,8 +37,6 @@ export function SlezakCard() {
                             </div>
                             <div class="absolute bottom-0 right-0 flex items-center gap-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 5 3" class="h-5 w-auto rounded-sm shadow-md"><rect width="5" height="3" fill="#FFCE00"></rect><rect width="5" height="2" fill="#DD0000"></rect><rect width="5" height="1" fill="#000"></rect></svg>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 3 2" class="h-5 w-auto rounded-sm shadow-md"><path fill="#fff" d="M0 0h3v2H0z"></path><path d="M1 0h1v2H1z" fill="#0033a0"></path><path d="M0 1h3v1H0z" fill="#d52b1e"></path></svg>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 30" class="h-5 w-auto rounded-sm shadow-md"><clipPath id="a-slezak"><path d="M30 15h30v15zv-15z"></path></clipPath><path d="M0 0v30h60V0z" fill="#012169"></path><path d="M0 0l60 30m0-30L0 30" stroke="#fff" stroke-width="6"></path><path d="M0 0l60 30m0-30L0 30" clip-path="url(#a-slezak)" stroke="#C8102E" stroke-width="4"></path><path d="M30 0v30M0 15h60" stroke="#fff" stroke-width="10"></path><path d="M30 0v30M0 15h60" stroke="#C8102E" stroke-width="6"></path></svg>
                             </div>
                         </div>
                     </div>
