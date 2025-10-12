265dab3af4f4514d517440655e9d1659223d970f
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-12 08:57:36 +0000

    Und ich sehe weiterhin alles in primary. Ich weiss also nicht wo Du gera

--- src/app/_components/cooperation-partners.tsx
+++ src/app/_components/cooperation-partners.tsx
@@ -52,7 +52,7 @@ const OtherPartnersGrid: React.FC<{ partners: OtherPartner[] }> = ({ partners })
         return (
             <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4">
                 {partners.map(partner => (
-                    <div key={partner.id} className="col-span-2">
+                    <div key={partner.id}>
                         <PartnerLink partner={partner} />
                     </div>
                 ))}
