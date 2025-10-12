a5d8bc4bd27ae06c64f7a6eaebd372d88f3baee4
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-12 00:22:44 +0000

    Nun müssen wir aber auf der Startseite noch ein paar Fehler beheben, die

--- src/app/_components/cooperation-partners.tsx
+++ src/app/_components/cooperation-partners.tsx
@@ -85,7 +85,7 @@ export function CooperationPartnersSection() {
                     rel="noopener noreferrer"
                     className="group relative block h-32 w-full overflow-hidden rounded-lg shadow-lg focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2"
                   >
-                    <Card className="flex h-full w-full items-center p-6">
+                    <Card className="flex h-full w-full items-center bg-background p-6">
                       <CardContent className="flex w-full items-center justify-center p-0">
                          <div className="relative flex h-[77px] w-full items-center justify-center overflow-hidden">
                             {renderPartnerLogo(partner)}
@@ -119,7 +119,7 @@ export function CooperationPartnersSection() {
                   rel="noopener noreferrer"
                   className="group relative block h-32 w-full overflow-hidden rounded-lg shadow-lg focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2"
                 >
-                  <Card className="flex h-full w-full items-center justify-center p-2">
+                  <Card className="flex h-full w-full items-center justify-center bg-background p-2">
                     <CardContent className="relative flex w-full h-full items-center justify-center p-0 overflow-hidden">
                         <Image
                           src={partner.logoUrl!}
