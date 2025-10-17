539fc34bb65c853f800943d22d2e4f75190e4643
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-17 07:10:53 +0000

    Das hat nicht funktioniert. Was mich vor allem wundert ist, dass Du auch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { Footer } from './_components/footer';
 import { Hero } from './_components/hero';
 import { WelcomeSection } from './_components/welcome-section';
 import { QuickNavSection } from './_components/quick-nav-section';
-import { CooperationPartnersSection } from './_components/cooperation-partners';
 
 
 export default function Home() {
@@ -15,7 +14,13 @@ export default function Home() {
         <Hero />
         <WelcomeSection />
         <QuickNavSection />
-        <CooperationPartnersSection />
+        <section id="partners" className="w-full bg-green-600">
+            <div className="mx-auto w-full px-4 pt-12 pb-16 sm:px-6 lg:px-8">
+                <h2 className="text-center font-headline text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                    Unsere ärztlichen Kooperationspartner
+                </h2>
+            </div>
+        </section>
       </main>
       <Footer />
     </div>
