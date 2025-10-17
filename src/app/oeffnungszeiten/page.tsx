a261cb6d9130cee9389f00125c2f0289f2263dee
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-10-17 14:07:35 +0000

    Schreibe mir nun noch in die beiden Seiten mit Öffnungs- und Telefonzeit

--- src/app/oeffnungszeiten/page.tsx
+++ src/app/oeffnungszeiten/page.tsx
@@ -13,6 +13,9 @@ export default function OeffnungszeitenPage() {
                 <h2 className="font-headline text-3xl font-bold tracking-tight text-primary sm:text-4xl sm:whitespace-nowrap">
                     ÖFFNUNGSZEITEN
                 </h2>
+                <p className="mt-6 text-lg text-foreground/80">
+                    Bitte beachten Sie, dass die Telefon- und Öffnungszeiten voneinander abweichen.
+                </p>
             </div>
 
             <div className="mx-auto mt-16">
--- src/app/telefonzeiten/page.tsx
+++ src/app/telefonzeiten/page.tsx
@@ -13,6 +13,9 @@ export default function TelefonzeitenPage() {
                 <h2 className="font-headline text-3xl font-bold tracking-tight text-primary sm:text-4xl sm:whitespace-nowrap">
                     TELEFONZEITEN
                 </h2>
+                 <p className="mt-6 text-lg text-foreground/80">
+                    Bitte beachten Sie, dass die Telefon- und Öffnungszeiten voneinander abweichen.
+                </p>
             </div>
 
             <div className="mx-auto mt-16">
