0b49664de99d0aa53e058707a1057b4721424efd
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-11-07 22:55:00 +0000

    ist das hier möglich bei meiner Seite: Passe deine next.config.js Datei

---
+++ src/app/next.config.ts
@@ -0,0 +1,28 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  // Suppress the warning about source maps in production, as it's not critical for this setup.
+  productionBrowserSourceMaps: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+      },
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      }
+    ],
+  },
+};
+
+export default nextConfig;
