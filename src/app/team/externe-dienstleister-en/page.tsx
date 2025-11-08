f6a778dfcf83117aa4ed0a5a8c0b3b726844b15f
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-11-08 23:02:33 +0000

    Ich habe nun diesen Fehler. Ich erwarte dass wir diesen zunächst besprec

--- src/app/team/externe-dienstleister-en/page.tsx
+++ src/app/team/externe-dienstleister-en/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import ClientLayout from '../../../_components/ClientLayout';
+import ClientLayout from '../../_components/ClientLayout';
 import ExterneDienstleisterContent from '@/app/team/_components/externe-dienstleister-content';
 
 export default function ExterneDienstleisterEnPage() {
