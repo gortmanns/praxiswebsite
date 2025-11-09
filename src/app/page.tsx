e0121fccaa552b6839c73c3a7a729d1ba9c961da
Gernot Ortmanns <gernot.ortmanns@gmail.com>
2025-11-09 12:47:09 +0000

    Wir starten mit der Basisseite. Nun kommentieren wir alle includes aus.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
 'use client';
-import { Hero } from './_components/hero';
-import { WelcomeSection } from './_components/welcome-section';
-import { QuickNavSection } from './_components/quick-nav-section';
-import { CooperationPartnersSection } from './_components/cooperation-partners';
+// import { Hero } from './_components/hero';
+// import { WelcomeSection } from './_components/welcome-section';
+// import { QuickNavSection } from './_components/quick-nav-section';
+// import { CooperationPartnersSection } from './_components/cooperation-partners';
 import ClientLayout from './_components/ClientLayout';
 
 export default function Home() {
   return (
     <ClientLayout>
-      <Hero isEnglish={false} />
-      <WelcomeSection isEnglish={false} />
-      <QuickNavSection />
-      <CooperationPartnersSection isEnglish={false} />
+      {/* <Hero isEnglish={false} /> */}
+      {/* <WelcomeSection isEnglish={false} /> */}
+      {/* <QuickNavSection /> */}
+      {/* <CooperationPartnersSection isEnglish={false} /> */}
     </ClientLayout>
   );
 }
